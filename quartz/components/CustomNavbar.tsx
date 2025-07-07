b59b301379d70250e87aacc1625cf98592b172a7
bendwild <dewildebenjamin0@gmail.com>
2025-07-07 14:21:41 +0200

    Update CustomNavbar.tsx

--- quartz/components/CustomNavbar.tsx
+++ quartz/components/CustomNavbar.tsx
@@ -1,23 +1,25 @@
 import { QuartzComponent, QuartzComponentConstructor } from "../types"
 
 const CustomNavbar: QuartzComponent = () => {
+  const BASE_PATH = "/afromedica"
+
   return (
     <nav className="main-navigation">
       <div className="nav-container">
         <div className="nav-logo">
-          <a href="/index">
+          <a href={`${BASE_PATH}/index`}>
             <img
-              src="https://raw.githubusercontent.com/bendwild/afro/v4/content/Extra/Images/afromedica%20(6).png"
+              src="https://raw.githubusercontent.com/bendwild/afromedica/v4/content/Extra/Images/afromedica%20(6).png"
               alt="AfroMedica Logo"
             />
           </a>
         </div>
         <ul className="nav-menu">
-          <li><a href="/about" className="nav-link">About Us</a></li>
-          <li><a href="/afrocademy" className="nav-link">Afrocademy</a></li>
-          <li><a href="/projects" className="nav-link">Projects</a></li>
-          <li><a href="/team" className="nav-link">Team</a></li>
-          <li><a href="/contact" className="nav-link">Contact</a></li>
+          <li><a href={`${BASE_PATH}/about`} className="nav-link">About Us</a></li>
+          <li><a href={`${BASE_PATH}/afrocademy`} className="nav-link">Afrocademy</a></li>
+          <li><a href={`${BASE_PATH}/projects`} className="nav-link">Projects</a></li>
+          <li><a href={`${BASE_PATH}/team`} className="nav-link">Team</a></li>
+          <li><a href={`${BASE_PATH}/contact`} className="nav-link">Contact</a></li>
         </ul>
       </div>
     </nav>
