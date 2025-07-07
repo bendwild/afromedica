08e76058d4e5a2f82b78b8f783195bf14cfedbb4
bendwild <dewildebenjamin0@gmail.com>
2025-07-07 14:19:36 +0200

    Update CustomNavbar.tsx

--- quartz/components/CustomNavbar.tsx
+++ quartz/components/CustomNavbar.tsx
@@ -5,7 +5,7 @@ const CustomNavbar: QuartzComponent = () => {
     <nav className="main-navigation">
       <div className="nav-container">
         <div className="nav-logo">
-          <a href="afromedica/index">
+          <a href="/index">
             <img
               src="https://raw.githubusercontent.com/bendwild/afro/v4/content/Extra/Images/afromedica%20(6).png"
               alt="AfroMedica Logo"
@@ -13,11 +13,11 @@ const CustomNavbar: QuartzComponent = () => {
           </a>
         </div>
         <ul className="nav-menu">
-          <li><a href="afromedica/about" className="nav-link">About Us</a></li>
-          <li><a href="afromedica/afrocademy" className="nav-link">Afrocademy</a></li>
-          <li><a href="afromedica/projects" className="nav-link">Projects</a></li>
-          <li><a href="afromedica/team" className="nav-link">Team</a></li>
-          <li><a href="afromedica/contact" className="nav-link">Contact</a></li>
+          <li><a href="/about" className="nav-link">About Us</a></li>
+          <li><a href="/afrocademy" className="nav-link">Afrocademy</a></li>
+          <li><a href="/projects" className="nav-link">Projects</a></li>
+          <li><a href="/team" className="nav-link">Team</a></li>
+          <li><a href="/contact" className="nav-link">Contact</a></li>
         </ul>
       </div>
     </nav>
