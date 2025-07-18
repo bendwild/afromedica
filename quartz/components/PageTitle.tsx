8999b39cd7cf7ec1910fbfcb742a1b0edf4bc7cc
bendwild <dewildebenjamin0@gmail.com>
2025-07-18 23:27:42 +0200

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,12 +7,9 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <div class={classNames(displayClass, "page-title-container")}>
-      <h1 class="page-title">
-        <img src="https://github.com/bendwild/afromedica/blob/v4/content/Extra/Images/afromedica (6).png?raw=true" alt="Logo"></img>
-        <a href={baseDir}>{title}</a>
-      </h1>
-    </div>
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>{title}</a>
+    </h2>
   )
 }
 
@@ -20,6 +17,7 @@ PageTitle.css = `
 .page-title {
   font-size: 1.75rem;
   margin: 0;
+  font-family: var(--titleFont);
 }
 `
 
