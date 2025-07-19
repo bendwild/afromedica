0b0931a6eacfb56e51fafd65d93ef51ee6f7044c
bendwild <dewildebenjamin0@gmail.com>
2025-07-20 00:10:22 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,24 +1,54 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import CustomNavbar from "./quartz/components/CustomNavbar" // 👈 Import your navbar
+import CustomNavbar from "./quartz/components/CustomNavbar"
 
-// Components shared across all pages
+// Inline simple SVG icon components for demo purposes
+const LinkedInIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+    <path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7 0h4.8v2.3h.07c.67-1.3 2.3-2.7 4.73-2.7 5.06 0 6 3.33 6 7.66V24h-5v-7.66c0-1.83-.03-4.2-2.56-4.2-2.57 0-2.97 2-2.97 4.07V24H7V8z"/>
+  </svg>
+)
+
+const FacebookIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+    <path d="M22.675 0h-21.35C.6 0 0 .6 0 1.325v21.351C0 23.4.6 24 1.325 24h11.495v-9.294H9.691v-3.622h3.129V8.413c0-3.1 1.894-4.788 4.659-4.788 1.325 0 2.463.099 2.794.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 1.764v2.312h3.587l-.467 3.622h-3.12V24h6.116c.725 0 1.325-.6 1.325-1.324V1.325C24 .6 23.4 0 22.675 0z"/>
+  </svg>
+)
+
+const InstagramIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+    <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5a4.25 4.25 0 004.25-4.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7zm4.75-.875a1.125 1.125 0 11-2.25 0 1.125 1.125 0 012.25 0z"/>
+  </svg>
+)
+
+const DiscordIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+    <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0786-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0811.0095c.1192.099.246.1981.3727.2924a.0766.0766 0 01-.0064.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1062c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.082.082 0 00.03-.0552c.5-5.177-.8382-9.6739-3.5485-13.66a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2107 0 2.1756 1.0952 2.1567 2.419 0 1.3332-.946 2.4189-2.1567 2.4189z"/>
+  </svg>
+)
+
+const MailIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+    <path d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 2v.01L12 13 4 6.01V6h16zM4 18v-9.57l7.387 6.425a1 1 0 001.226 0L20 8.43V18H4z"/>
+  </svg>
+)
+
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [CustomNavbar()], // 👈 Add navbar here
+  header: [CustomNavbar()],
   afterBody: [],
   footer: Component.Footer({
     links: {
-      LinkedIn: "https://www.linkedin.com/company/afromedica/?viewAsMember=true",
-      Facebook: "https://www.facebook.com/AfroMedica",
-      Instagram: "https://www.instagram.com/_afromedica_/?hl=nl",
-      "Discord Community": "https://discord.gg/qUcCAHassB",
-      Mail: "mailto:info@afromedica.com",
+      "https://www.linkedin.com/company/afromedica/?viewAsMember=true": <LinkedInIcon />,
+      "https://www.facebook.com/AfroMedica": <FacebookIcon />,
+      "https://www.instagram.com/_afromedica_/?hl=nl": <InstagramIcon />,
+      "https://discord.gg/qUcCAHassB": <DiscordIcon />,
+      "mailto:info@afromedica.com": <MailIcon />,
     },
   }),
 }
 
-// Components for individual content pages (notes, pages)
+// ... rest of your layouts unchanged
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
@@ -51,7 +81,6 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// Components for list pages (tags, folders, etc.)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
