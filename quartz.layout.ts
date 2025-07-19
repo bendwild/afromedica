104178c1c0617d22853a2675fe5266504f15d879
bendwild <dewildebenjamin0@gmail.com>
2025-07-19 14:13:25 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,6 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
           Component: Component.Search(),
           grow: true,
         },
+        { Component: Component.Darkmode() },
         { Component: Component.ReaderMode() },
       ],
     }),
