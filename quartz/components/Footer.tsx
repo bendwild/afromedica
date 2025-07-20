51cd3f98c87c6fe7a2435166d1f2dd704b064432
bendwild <dewildebenjamin0@gmail.com>
2025-07-20 22:35:26 +0200

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,8 +1,17 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 
+// Copy your icons.svg content here
+const sprite = `
+  <symbol id="linkedin" viewBox="0 0 24 24">...</symbol>
+  <symbol id="facebook" viewBox="0 0 24 24">...</symbol>
+  <symbol id="instagram" viewBox="0 0 24 24">...</symbol>
+  <symbol id="discord" viewBox="0 0 24 24">...</symbol>
+  <symbol id="mail" viewBox="0 0 24 24">...</symbol>
+`
+
 interface Options {
-  links: Record<string, string> // key = URL, value = icon id (e.g. "linkedin")
+  links: Record<string, string> // key = URL, value = icon id
 }
 
 export default ((opts?: Options) => {
@@ -11,12 +20,15 @@ export default ((opts?: Options) => {
 
     return (
       <footer class={`${displayClass ?? ""}`}>
+        {/* Inject symbols once, hidden */}
+        <svg style="display: none;" dangerouslySetInnerHTML={{ __html: sprite }} />
+
         <ul class="social-icons">
           {Object.entries(links).map(([url, iconId]) => (
             <li key={url}>
               <a href={url} target="_blank" rel="noopener noreferrer" aria-label={iconId}>
                 <svg>
-                  <use href={`./icons.svg#${iconId}`} />
+                  <use href={`#${iconId}`} />
                 </svg>
               </a>
             </li>
