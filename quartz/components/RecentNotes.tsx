bb22a479a123f016cf7ac42341c6492627713695
bendwild <dewildebenjamin0@gmail.com>
2025-07-21 22:45:34 +0200

    Update RecentNotes.tsx

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -19,7 +19,7 @@ interface Options {
 
 const defaultOptions = (cfg: GlobalConfiguration): Options => ({
   limit: 5
-  linkToMore: true,
+  linkToMore: "Updates",
   showTags: true,
   filter: () => true,
   sort: byDateAndAlphabetical(cfg),
