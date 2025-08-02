06c72f3eec62ab08b88bf642874f1fad35e442fa
bendwild <dewildebenjamin0@gmail.com>
2025-08-02 13:47:59 +0200

    Update CustomNavbar.tsx

--- quartz/components/CustomNavbar.tsx
+++ quartz/components/CustomNavbar.tsx
@@ -1,12 +1,11 @@
 import { QuartzComponent, QuartzComponentConstructor } from "../types"
 
 const CustomNavbar: QuartzComponent = () => {
-  
   return (
     <nav className="main-navigation">
       <div className="nav-container">
         <div className="nav-logo">
-          <a href={`${BASE_PATH}/index`}>
+          <a href="/index">
             <img
               src="https://raw.githubusercontent.com/bendwild/afromedica/v4/content/Extra/Images/afromedica%20(6).png"
               alt="AfroMedica Logo"
@@ -14,13 +13,13 @@ const CustomNavbar: QuartzComponent = () => {
           </a>
         </div>
         <ul className="nav-menu">
-          <li><a href={`${BASE_PATH}/About-us/about-us`} className="nav-link">About Us</a></li>
-          <li><a href={`${BASE_PATH}/Afromedica-Academy/Afromedica-Academy`} className="nav-link">AfroAcademy</a></li>
-          <li><a href={`${BASE_PATH}/Afromedica-Talks/Afromedica-Talks`} className="nav-link">Afromedica Talks</a></li>
-          <li><a href={`${BASE_PATH}/Afromedica-Connects/Afromedica-Connects`} className="nav-link">Afromedica Connects</a></li>
-          <li><a href={`${BASE_PATH}/Policy/policy`} className="nav-link">Policy</a></li>
-          <li><a href={`${BASE_PATH}/Team/team`} className="nav-link">Team</a></li>
-          <li><a href={`${BASE_PATH}/Contact/contact`} className="nav-link">Contact</a></li>
+          <li><a href="/About-us/about-us" className="nav-link">About Us</a></li>
+          <li><a href="/Afromedica-Academy/Afromedica-Academy" className="nav-link">AfroAcademy</a></li>
+          <li><a href="/Afromedica-Talks/Afromedica-Talks" className="nav-link">Afromedica Talks</a></li>
+          <li><a href="/Afromedica-Connects/Afromedica-Connects" className="nav-link">Afromedica Connects</a></li>
+          <li><a href="/Policy/policy" className="nav-link">Policy</a></li>
+          <li><a href="/Team/team" className="nav-link">Team</a></li>
+          <li><a href="/Contact/contact" className="nav-link">Contact</a></li>
         </ul>
       </div>
     </nav>
