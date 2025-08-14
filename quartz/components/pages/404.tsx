2fb4b7abf12160463ff33d8caa505f3247f6dfed
bendwild <dewildebenjamin0@gmail.com>
2025-08-14 13:20:38 +0200

    Update 404.tsx

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -2,14 +2,17 @@ import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
-  const homepage = `/${cfg.locale}/` // sends user to the localized home
+  const homepage = "/"
+
   const t = i18n(cfg.locale)
 
   return (
     <article className="popover-hint">
       <h1>404</h1>
-      <p>{String(t.pages?.error?.notFound ?? "Page not found")}</p>
-      <a href={homepage}>{String(t.pages?.error?.home ?? "Go home")}</a>
+      <p>{String(t?.pages?.error?.notFound || "Page not found")}</p>
+      <a href={homepage}>
+        {String(t?.pages?.error?.home || "Go back home")}
+      </a>
     </article>
   )
 }
