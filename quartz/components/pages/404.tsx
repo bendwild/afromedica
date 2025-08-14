5dd747229e4e8a52951231ce6951d5f73634bd95
bendwild <dewildebenjamin0@gmail.com>
2025-08-14 12:48:12 +0200

    Update 404.tsx

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -2,10 +2,11 @@ import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
-  const homepage = "https://bendwild.github.io/afromedica/index"
+  // Use cfg.baseUrl instead of hardcoding
+  const homepage = `${cfg.baseUrl}/${cfg.locale}/index`
 
   return (
-    <article class="popover-hint">
+    <article className="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={homepage}>{i18n(cfg.locale).pages.error.home}</a>
