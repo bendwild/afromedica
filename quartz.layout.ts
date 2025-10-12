68588d4d83e58f3b17334567f7c153fe0ea19082
bendwild <dewildebenjamin0@gmail.com>
2025-10-12 14:23:23 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,19 +1,21 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 import CustomNavbar from "./quartz/components/CustomNavbar" // 👈 Import your navbar
+
 // Components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [CustomNavbar()], // 👈 Add navbar here
   afterBody: [],
   footer: Component.Footer({
-  links: {
-    "https://www.linkedin.com/company/afromedica/?viewAsMember=true": "linkedin",
-    "https://www.facebook.com/AfroMedica": "facebook",
-    "https://www.instagram.com/_afromedica_/?hl=nl": "instagram",
-  },
-}),
+    links: {
+      "https://www.linkedin.com/company/afromedica/?viewAsMember=true": "linkedin",
+      "https://www.facebook.com/AfroMedica": "facebook",
+      "https://www.instagram.com/_afromedica_/?hl=nl": "instagram",
+    },
+  }),
 }
+
 // Components for individual content pages (notes, pages)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
@@ -39,11 +41,9 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.RecentNotes(),
   ],
-  right: [
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  right: [], // 👈 Removed TableOfContents and Backlinks
 }
+
 // Components for list pages (tags, folders, etc.)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [
@@ -64,5 +64,5 @@ export const defaultListPageLayout: PageLayout = {
     }),
     Component.Explorer(),
   ],
-  right: [],
+  right: [], // 👈 Already empty
 }
