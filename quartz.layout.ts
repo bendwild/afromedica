14568bd8dfb983e4c40266a2bb8d829976eeb038
bendwild <dewildebenjamin0@gmail.com>
2025-10-27 04:08:06 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,14 +45,6 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-      ],
-    }),
     Component.Explorer(),
   ],
   right: [],
