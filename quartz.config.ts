ca59c3004dc83959a13e2146eb4b3f26cf300d4f
bendwild <dewildebenjamin0@gmail.com>
2026-02-01 21:18:09 -0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+    provider: "google",
+    tagId: "G-MNG9VDJPK1",
     },
     locale: "en-US",
     baseUrl: "https://afromedica.be",
